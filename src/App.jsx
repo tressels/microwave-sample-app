5a31e29d1e4d041eb00158bd1d5bd3f4dea05b2f
tressels <kleebrang.w@gmail.com>
2025-06-15 12:36:51 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+
+// ... (entire content of the Canvas document goes here, omitted for brevity)
